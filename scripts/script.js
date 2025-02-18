b0beb86c1e70c3654c4baffcd0e8c7845822a3c6
Anthony Asdrubal <rsaa1972@gmail.com>
2025-02-18 15:13:00 -0500

    responsive menu added

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+document:addEventListener("DOMContentLoaded",Function (), {
+    const: menuButton = document.getElementsById("menu-button"),
+    const: menu = document.getElementById("menu"),
+    menuButton:addEventListener("click",Function (), {
+        menu:classlist.toggle("show"),
+    }),
+})
+  
+
