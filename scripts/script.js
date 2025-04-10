cd0c618b73b9655ea5327cbf59d86a44ec00ae63
Anthony Asdrubal <rsaa1972@gmail.com>
2025-04-10 09:20:04 -0400

    adecuaciones menu desplegable

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
-
 window.addEventListener("load", () => {
     const menuButton = document.getElementById("menu-button");
     const menu = document.getElementById("menu");
+    const header = document.getElementById("header");
 
     menuButton.addEventListener("click", () => {
      menuButton.classList.toggle("active");
      menu.classList.toggle("menuOpen");
+     header.classList.toggle("menuOpen");
    });
 })
